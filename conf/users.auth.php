e32350e1b5b6821962f8f7c0c10d8947fb8b40f0
fansongy <fansongy@126.com>
2015-06-05 20:37:27 +0800

    init wiki base

---
+++ conf/users.auth.php
@@ -0,0 +1,13 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+fansy:$1$5R5qGo4f$3r5r4NBtV2edhKZqDvqiG1:SongYang:fansongy@gmail.com:admin,user
+super7529:$1$KLtLcHao$1kbTxEtBGeXuPMNQ/Mpnj1:super7529:super7529@126.com:user
