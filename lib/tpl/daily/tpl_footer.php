d44f8a56be2cc04fdbc64021ee673dfa91a14875
fansongy <fansongy@126.com>
2015-06-21 10:05:30 +0800

    update daily theme

--- lib/tpl/daily/tpl_footer.php
+++ lib/tpl/daily/tpl_footer.php
@@ -12,7 +12,7 @@ if (!defined('DOKU_INC')) die();
     <div class="pad">
         <?php tpl_license(''); // license text ?>
         <div class="wrap">
-            本站由<a href="http://weibo.com/fansongy" title="author"> @松阳 </a>基于<a href="http://www.dokuwiki.org" title="DokuWiki"> DokuWiki </a>搭建，使用主题为Daily。
+            本站由<a href="http://weibo.com/fansongy" title="author"> @松阳 </a>基于<a href="http://www.dokuwiki.org" title="DokuWiki"> DokuWiki </a>搭建，使用主题为<a href="https://www.dokuwiki.org/template:daily" title="Daily"> Daily </a>。
         </div>
     </div>
 </div><!-- /footer -->
